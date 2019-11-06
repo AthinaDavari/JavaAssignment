7b8faf737954ab34b1c9b04c813487fb8cb918aa
AthinaDavari <t8180020@aueb.gr>
2019-11-06 19:34:42 +0200

    Add static method printList

--- proderp/src/main/java/pijavaparty/proderp/main/Main.java
+++ proderp/src/main/java/pijavaparty/proderp/main/Main.java
@@ -1,6 +1,8 @@
 package pijavaparty.proderp.main;
 
+import java.util.List;
 import pijavaparty.proderp.dao.SupplierDao;
+import pijavaparty.proderp.entity.Supplier;
 
 /**
  *
@@ -10,7 +12,13 @@ public class Main {
     
     public static void main(String[] args) {
         SupplierDao supplierDao = new SupplierDao();
-        System.out.println(supplierDao.getAll());
+        printList(supplierDao.getAll());
     }
     
+    public static void printList(List<Supplier> a){
+        System.out.println(a.getClass());
+        for(int i=0; i < a.size(); i++){
+            System.out.println(a.get(i));
+        }
+    }
 }

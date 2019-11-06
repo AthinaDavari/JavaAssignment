9991555cdb67f4a6ae4a2999cd7686bd07996b08
Natalia Katsiapi <kat26natalia@gmail.com>
2019-11-06 13:24:40 +0200

    Add 2 inserts

--- erp_progII.sql
+++ erp_progII.sql
@@ -78,3 +78,10 @@ CREATE TABLE `P_Materials` (
   FOREIGN KEY (`product_id`) REFERENCES `Products` (`id`),
   FOREIGN KEY (`raw_material_id`) REFERENCES `Raw_Materials` (`id`)
 );
+
+
+
+-- INSERTS
+insert into suppliers(full_name, address, phonenumber, email) 
+values("SideroA.E.", "A.Papadreou 30", 2105678934, "info@sidero.gr"),
+	  ("PetaloudaA.E.", "Palaiologou 156", 2103789023, "info@petalouda.gr");

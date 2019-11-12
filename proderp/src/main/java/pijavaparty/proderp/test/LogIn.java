b2919e154762023292395d28787c634a1c830318
AggelikiPapa <56364200+AggelikiPapa@users.noreply.github.com>
2019-11-12 15:19:24 +0200

    LogIn class icon

--- proderp/src/main/java/pijavaparty/proderp/test/LogIn.java
+++ proderp/src/main/java/pijavaparty/proderp/test/LogIn.java
@@ -18,8 +18,11 @@ public class LogIn extends javax.swing.JFrame {
      */
     public LogIn() {
         initComponents();
+        seticon();
     }
-
+    private void seticon() {
+			     setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/logo.jpg")));
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always

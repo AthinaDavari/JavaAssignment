64174173fef67b73495c109b8a5ccab798f9776b
AnnaShambel3 <t8180112@aueb.gr>
2019-11-13 21:22:14 +0200

    Add Product Table, Raw Material Table and Associated Tables

--- proderp/src/main/java/pijavaparty/proderp/dao/AbstractDao.java
+++ proderp/src/main/java/pijavaparty/proderp/dao/AbstractDao.java
@@ -22,7 +22,7 @@ public abstract class AbstractDao<T> {
 
     protected static final String URL = "jdbc:mysql://localhost:3306/proderp?serverTimezone=UTC";
     protected static final String USERNAME = "root";
-    protected static final String PASS = "12345";
+    protected static final String PASS = "melodiabolzano33";
     private Connection conn;
 
     public Connection getConnection() {

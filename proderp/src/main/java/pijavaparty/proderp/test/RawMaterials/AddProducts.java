b3340e4ae0d0d7db0d9e16ad96958571e60df975
AnnaShambel3 <t8180112@aueb.gr>
2019-11-15 22:39:12 +0200

    Add Changes

--- proderp/src/main/java/pijavaparty/proderp/test/RawMaterials/AddProducts.java
+++ proderp/src/main/java/pijavaparty/proderp/test/RawMaterials/AddProducts.java
@@ -35,7 +35,7 @@ public class AddProducts extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Calibri", 1, 24)); // NOI18N
         jLabel1.setText("Add New Product");
--- proderp/src/main/java/pijavaparty/proderp/test/RawMaterials/Ingredients.java
+++ proderp/src/main/java/pijavaparty/proderp/test/RawMaterials/Ingredients.java
@@ -35,7 +35,7 @@ public class Ingredients extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         Ingredients_Table = new javax.swing.JTable();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setText("Product_Id:");
 

b96f5926f9fc8def1bab038dc1f2d7196d151493
AthinaDavari <t8180020@aueb.gr>
2019-12-14 20:01:35 +0200

    Add a method in UserDaoTest.java

--- proderp/src/test/java/pijavaparty/proderp/dao/UserDaoTest.java
+++ proderp/src/test/java/pijavaparty/proderp/dao/UserDaoTest.java
@@ -186,8 +186,6 @@ public class UserDaoTest {
         boolean expResult = true;
         boolean result = instance.permissionToDeleteAnAdministratorUser();
         assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }

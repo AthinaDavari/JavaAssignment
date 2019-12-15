4c51090c33b2043bc7348189857125441f6969de
AthinaDavari <t8180020@aueb.gr>
2019-12-15 14:52:57 +0200

    Add CustomerDaoTest.java

--- proderp/src/test/resources/test.sql
+++ proderp/src/test/resources/test.sql
@@ -40,3 +40,7 @@ values (1, "pending", "2019-12-14 20:02:43"),
        (2, "delivered", "2018-11-13 11:04:45"),
        (2, "pending", "2019-08-12 21:02:01"),
        (1, "delivered", "2019-08-16 09:02:43");
+insert into Customers (full_name,address,phonenumber,email)
+values ("Ora Gia Podilato", "Tositsa 45", 2109237849, "info@oragiapodilato.com"),
+       ("Mouzouris", "Markou 14", 2104534790, "info@mouzouris.com"),
+       ("BikeMall", "Patision 18", 2136789267, "info@bikemall.com");

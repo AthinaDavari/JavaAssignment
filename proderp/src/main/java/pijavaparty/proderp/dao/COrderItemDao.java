ee939d13d4f9f59af7fdc0d0c361c13e0f42cd9e
Athina Pantazopoulou <t8180089@aueb.gr>
2019-12-22 11:43:33 +0200

    Add documentation

--- proderp/src/main/java/pijavaparty/proderp/dao/COrderItemDao.java
+++ proderp/src/main/java/pijavaparty/proderp/dao/COrderItemDao.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pijavaparty.proderp.dao;
 
 import java.sql.PreparedStatement;
@@ -16,10 +11,12 @@ import java.util.logging.Logger;
 import pijavaparty.proderp.entity.COrderItem;
 
 /**
+ * COrderItemDao.java - a class for interacting and modifying the fields of a
+ * customer's items.
  *
  * @author Athina P.
+ * @see COrderItem
  */
-
 public class COrderItemDao extends Dao implements CompositeEntityI<COrderItem> {
 
     private static final String GETALL = "SELECT * FROM C_order_items";
@@ -30,6 +27,11 @@ public class COrderItemDao extends Dao implements CompositeEntityI<COrderItem> {
     private COrderDao co = new COrderDao();
     private ProductDao pr = new ProductDao();
 
+    /**
+     * Add new customer's items in a List.
+     *
+     * @return A COrder data type List.
+     */
     @Override
     public List<COrderItem> getAll() {
         List<COrderItem> corders = new LinkedList();
@@ -49,6 +51,13 @@ public class COrderItemDao extends Dao implements CompositeEntityI<COrderItem> {
         return corders;
     }
 
+    /**
+     * Return a customer's order item with a specific id.
+     *
+     * @param coid A variable of type int.
+     * @param prid A variable of type int.
+     * @return A COrderItem data type object.
+     */
     public COrderItem getByIds(int coid, int prid) {
         PreparedStatement pst = null;
         ResultSet rs = null;
@@ -68,6 +77,12 @@ public class COrderItemDao extends Dao implements CompositeEntityI<COrderItem> {
         return null;
     }
 
+    /**
+     * Add items sorted by customer.
+     *
+     * @param coid A variable of type int.
+     * @return A COrderItem data type List.
+     */
     public List<COrderItem> getItemsperCOrder(int coid) {
         List<COrderItem> coi = new LinkedList();
         PreparedStatement pst = null;
@@ -87,6 +102,11 @@ public class COrderItemDao extends Dao implements CompositeEntityI<COrderItem> {
         return coi;
     }
 
+    /**
+     * Insert a new customer's item.
+     *
+     * @param coi An object of type COrderItem.
+     */
     @Override
     public void insert(COrderItem coi) {
         PreparedStatement pst = null;
@@ -103,6 +123,11 @@ public class COrderItemDao extends Dao implements CompositeEntityI<COrderItem> {
         }
     }
 
+    /**
+     * Delete a customer's item with a specific id.
+     *
+     * @param coi A COrderItem data type object.
+     */
     public void delete(COrderItem coi) {
         PreparedStatement pst = null;
         try {
@@ -117,7 +142,13 @@ public class COrderItemDao extends Dao implements CompositeEntityI<COrderItem> {
             closeStatementAndResultSet(pst);
         }
     }
-    
+
+    /**
+     * Delete a customer's item with a specific id.
+     *
+     * @param cordId An int data type object.
+     * @param pId An int data type object.
+     */
     @Override
     public void delete(int cordId, int pId) {
         PreparedStatement pst = null;
@@ -133,6 +164,5 @@ public class COrderItemDao extends Dao implements CompositeEntityI<COrderItem> {
             closeStatementAndResultSet(pst);
         }
     }
-
 
 }

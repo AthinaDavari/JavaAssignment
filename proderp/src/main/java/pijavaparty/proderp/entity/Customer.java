c7c8ae34df7b816238309f56c68528099680bad8
Athina Pantazopoulou <t8180089@aueb.gr>
2019-12-22 11:13:43 +0200

    Add documentation

--- proderp/src/main/java/pijavaparty/proderp/entity/Customer.java
+++ proderp/src/main/java/pijavaparty/proderp/entity/Customer.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pijavaparty.proderp.entity;
 
 import java.util.Objects;
 
 /**
+ * Customer.java - a class with the necessary getters and setters for a Customer
+ * data type object.
  *
  * @author athinaDavari
  */
@@ -19,9 +16,20 @@ public class Customer {
     private Long phonenumber;
     private String email;
 
+    /**
+     * A no-argument Constructor.
+     */
     public Customer() {
     }
 
+    /**
+     * A constructor with arguments.
+     *
+     * @param fullName A variable of type String.
+     * @param address A variable of type String.
+     * @param phonenumber A variable of type Long.
+     * @param email A variable of type String.
+     */
     public Customer(String fullName, String address, Long phonenumber, String email) {
         this.fullName = fullName;
         this.address = address;
@@ -29,6 +37,15 @@ public class Customer {
         this.email = email;
     }
 
+    /**
+     * A constructor with arguments.
+     *
+     * @param id A variable of type int.
+     * @param fullName A variable of type String.
+     * @param address A variable of type String.
+     * @param phonenumber A variable of type Long.
+     * @param email A variable of type String.
+     */
     public Customer(int id, String fullName, String address, Long phonenumber, String email) {
         this.id = id;
         this.fullName = fullName;
@@ -37,46 +54,101 @@ public class Customer {
         this.email = email;
     }
 
+    /**
+     * Retrieve the value of id.
+     *
+     * @return An int data type.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Retrieve the value of fullname
+     *
+     * @return A String data type.
+     */
     public String getFullName() {
         return fullName;
     }
 
+    /**
+     * Retrieve the value of address.
+     *
+     * @return A String data type.
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Retrieve the value of phonenumber.
+     *
+     * @return A Long data type.
+     */
     public Long getPhonenumber() {
         return phonenumber;
     }
 
+    /**
+     * Retrieve the value of email.
+     *
+     * @return A String data type.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Set the value of id.
+     *
+     * @param id A variable of type int.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Set the value of fullname.
+     *
+     * @param fullName A variable of type String.
+     */
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
+    /**
+     * Set the value of address.
+     *
+     * @param address A variable of type String.
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+     * Set the value of phonenumber.
+     *
+     * @param phonenumber A variable of type Long.
+     */
     public void setPhonenumber(Long phonenumber) {
         this.phonenumber = phonenumber;
     }
 
+    /**
+     * Set the value of email.
+     *
+     * @param email A variable of type String.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Return the hash code value for this set.
+     *
+     * @return An int data type.
+     */
     @Override
     public int hashCode() {
         int hash = 3;
@@ -88,6 +160,12 @@ public class Customer {
         return hash;
     }
 
+    /**
+     * Compare the specified object with this set for equality.
+     *
+     * @param obj A variable of type Object.
+     * @return A boolean data type.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -118,6 +196,11 @@ public class Customer {
         return true;
     }
 
+    /**
+     * Return a string representation of this collection.
+     *
+     * @return A String data type.
+     */
     @Override
     public String toString() {
         return "Customer{" + "id=" + id + ", fullName=" + fullName + ", address=" + address + ", phonenumber=" + phonenumber + ", email=" + email + '}';

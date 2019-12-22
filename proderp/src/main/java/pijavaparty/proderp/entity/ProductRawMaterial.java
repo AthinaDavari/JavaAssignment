5e99d6543b5154a273adb6f9edec284fa21b4e53
Athina Pantazopoulou <t8180089@aueb.gr>
2019-12-22 15:27:01 +0200

    Add documentation

--- proderp/src/main/java/pijavaparty/proderp/entity/ProductRawMaterial.java
+++ proderp/src/main/java/pijavaparty/proderp/entity/ProductRawMaterial.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pijavaparty.proderp.entity;
 
 import java.util.Objects;
 
 /**
- *
+ * ProductRawMaterial.java - a class with the necessary getters and setters for
+ * a ProductRawMaterial data type object.  *
  * @author Natalia
  */
 public class ProductRawMaterial {
@@ -17,39 +13,84 @@ public class ProductRawMaterial {
     private Product product;
     private RawMaterial rawMaterial;
 
+    /**
+     * A no-argument Constructor.
+     */
     public ProductRawMaterial() {
     }
 
+    /**
+     * A constructor with arguments.
+     *
+     * @param product A variable of type Product.
+     * @param rawMaterial A variable of type RawMaterial.
+     * @param quantityOfRawMaterial A variable of type int.
+     */
     public ProductRawMaterial(Product product, RawMaterial rawMaterial, int quantityOfRawMaterial) {
         this.product = product;
         this.rawMaterial = rawMaterial;
         this.quantityOfRawMaterial = quantityOfRawMaterial;
     }
 
+    /**
+     * Retrieve the value of quantityOfRawMaterial.
+     *
+     * @return An int data type.
+     */
     public int getQuantityOfRawMaterial() {
         return quantityOfRawMaterial;
     }
 
+    /**
+     * Set the value of quantityOfRawMaterial.
+     *
+     * @param quantityOfRawMaterial A variable of type int.
+     */
     public void setQuantityOfRawMaterial(int quantityOfRawMaterial) {
         this.quantityOfRawMaterial = quantityOfRawMaterial;
     }
 
+    /**
+     * Retrieve the value of product.
+     *
+     * @return A Product data type.
+     */
     public Product getProduct() {
         return product;
     }
 
+    /**
+     * Set the value of product.
+     *
+     * @param product A variable of type Product.
+     */
     public void setProduct(Product product) {
         this.product = product;
     }
 
+    /**
+     * Retrieve the value of rawMaterial.
+     *
+     * @return A RawMaterial data type.
+     */
     public RawMaterial getRawMaterial() {
         return rawMaterial;
     }
 
+    /**
+     * Set the value of rawMaterial.
+     *
+     * @param rawMaterial A variable of type RawMaterial.
+     */
     public void setRawMaterial(RawMaterial rawMaterial) {
         this.rawMaterial = rawMaterial;
     }
 
+    /**
+     * Return the hash code value for this set.
+     *
+     * @return An int data type.
+     */
     @Override
     public int hashCode() {
         int hash = 7;
@@ -59,6 +100,12 @@ public class ProductRawMaterial {
         return hash;
     }
 
+    /**
+     * Compare the specified object with this set for equality.
+     *
+     * @param obj A variable of type Object.
+     * @return A boolean data type.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -83,9 +130,14 @@ public class ProductRawMaterial {
         return true;
     }
 
+    /**
+     * Return a string representation of this collection.
+     *
+     * @return A String data type.
+     */
     @Override
     public String toString() {
         return "ProductRawMaterial{" + "quantityOfRawMaterial=" + quantityOfRawMaterial + ", product=" + product + ", rawMaterial=" + rawMaterial + '}';
     }
-    
+
 }

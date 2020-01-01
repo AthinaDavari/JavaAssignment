46c1f8eba8ea473c1c46cebaf7054cb76d55c827
Natalia Katsiapi <kat26natalia@gmail.com>
2020-01-01 20:09:06 +0200

    Add permissionToDecreaseRawMaterials at StorageService

--- proderp/src/main/java/gr/aueb/dmst/pijavaparty/proderp/services/StorageServices.java
+++ proderp/src/main/java/gr/aueb/dmst/pijavaparty/proderp/services/StorageServices.java
@@ -7,6 +7,9 @@ import gr.aueb.dmst.pijavaparty.proderp.dao.RawMaterialDao;
 import gr.aueb.dmst.pijavaparty.proderp.entity.COrderItem;
 import gr.aueb.dmst.pijavaparty.proderp.entity.Product;
 import gr.aueb.dmst.pijavaparty.proderp.entity.ProductRawMaterial;
+import gr.aueb.dmst.pijavaparty.proderp.entity.RawMaterial;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.function.Function;
 import javax.swing.JOptionPane;
@@ -24,31 +27,30 @@ public class StorageServices {
 
     }
 
-
     /**
      * Update the quantity of all the ingredients in product's recipe
+     *
      * @param id-the product's id we want to made
-     * @param quant-the quantity of product we want to make(with -) or destroy(with +)
-     * @return if the quantity of product's, return true,
-     * else false
+     * @param quant-the quantity of product we want to make(with -) or
+     * destroy(with +)
+     * @return if the quantity of product's, return true, else false
      */
-
     public boolean updateIngredients(int id, int quant) {
         ProductRawMaterialDao prodrawdao = new ProductRawMaterialDao();
-        
+
         //list of raw materials,which are included in product's recipe
         List<ProductRawMaterial> prodrawlist = prodrawdao.getMaterialsPerProduct(id);
         RawMaterialDao rawmatdao = new RawMaterialDao();
         Function<Integer, Integer> f = updateQuantityOfRawMaterialBy(quant);
-        
+
         //if we want to make products,check the raw materials pieces in storage
-        if(quant < 0){//start 1st if
-        //if at least a raw material has not enough pieces in storage, stop the process 
-        if (permissionToUpdateIngredients(id, quant) == false) {//start 2nd if
-            return false;
-        }//end 2nd if
+        if (quant < 0) {//start 1st if
+            //if at least a raw material has not enough pieces in storage, stop the process 
+            if (permissionToUpdateIngredients(id, quant) == false) {//start 2nd if
+                return false;
+            }//end 2nd if
         }//end 1st if
-        
+
         int prodrawlistsize = prodrawlist.size();//number of raw materials in product's recipe
         ProductRawMaterial prodraw;
         int quantrecipe;
@@ -69,11 +71,13 @@ public class StorageServices {
     }
 
     /**
-     * Check if the storage have enough raw materials to make a product in quantity we want
+     * Check if the storage have enough raw materials to make a product in
+     * quantity we want
+     *
      * @param id-the product's id we want to made
      * @param quant-the quantity of product we want to make(with -)
-     * @return if the storage have enough raw materials to make a product in quantity we want, return true,
-     * else false
+     * @return if the storage have enough raw materials to make a product in
+     * quantity we want, return true, else false
      */
     public boolean permissionToUpdateIngredients(int id, int quant) {
         ProductRawMaterialDao prodrawdao = new ProductRawMaterialDao();
@@ -82,7 +86,7 @@ public class StorageServices {
         List<ProductRawMaterial> prodrawlist = prodrawdao.getMaterialsPerProduct(id);
         Function<Integer, Integer> f = updateQuantityOfRawMaterialBy(quant);
         int prodrawlistsize = prodrawlist.size();//number of raw materials in product's recipe
-        
+
         int quantrecipe;
         int rawquant;
         for (int i = 0; i < prodrawlistsize; i++) {//start for
@@ -93,34 +97,67 @@ public class StorageServices {
             //raw material's quantity in storage
             rawquant = prodraw.getRawMaterial().getQuantity();
             rawquant += f.apply(quantrecipe);//same with: rawquant = rawquant + quantrecipe * quant;
-            
+
             //if the raw material has not enough pieces in storage, return false
             if (rawquant < 0) {
                 return false;
             }//end if
-            
+
         }//end for
-        
+
+        return true;
+    }
+
+    /**
+     * Creates a HashMap with total quantities of raw materials of a COrder and
+     * checks if raw materials can be decreased by this quantity
+     *
+     * @param corderid COrder's id
+     * @return false - if at least one RawMaterial is insufficient, true - if
+     * all quantities are sufficient
+     */
+    public boolean permissionToDecreaseRawMaterials(int corderid) {
+        ProductRawMaterialDao prd = new ProductRawMaterialDao();
+        COrderItemDao coid = new COrderItemDao();
+        List<COrderItem> corderitems = coid.getItemsPerCOrder(corderid);
+        HashMap<RawMaterial, Integer> totalRawNeeded = new HashMap();
+        for (COrderItem cOrderItem : corderitems) {
+            ArrayList<ProductRawMaterial> rawsOfProduct = prd.getMaterialsPerProduct(cOrderItem.getProduct().getId());
+            for (ProductRawMaterial prm : rawsOfProduct) {
+                RawMaterial rawMaterial = prm.getRawMaterial();
+                /* get old value of quantity contained in HashMap and increase it by 
+                the other Product's  needed quantity for this particular RawMaterial */
+                totalRawNeeded.put(rawMaterial, totalRawNeeded.get(rawMaterial) + prm.getQuantityOfRawMaterial());
+            }
+        }
+        for (RawMaterial rm : totalRawNeeded.keySet()) {
+            int oldQuantity = rm.getQuantity();
+            Function<Integer, Integer> f = updateQuantityOfRawMaterialBy(oldQuantity);
+            int newQuantity = oldQuantity + f.apply((-1) * totalRawNeeded.get(rm));
+            if (newQuantity < 0) {
+                return false;
+            }
+        }
         return true;
     }
 
     /**
      *
-     * @param q-the quantity of product we want to make(with -) or destroy(with +)
+     * @param q-the quantity of product we want to make(with -) or destroy(with
+     * +)
      * @return
      */
     public Function<Integer, Integer> updateQuantityOfRawMaterialBy(int q) {
         return x -> q * x;
     }
 
-    
     /**
-     * Increase the quantities of a specific product that are available for 
+     * Increase the quantities of a specific product that are available for
      * customers and update this data in the database.
-     * 
-     * @param corderid - An integer that defines the id of an order from customers.
+     *
+     * @param corderid - An integer that defines the id of an order from
+     * customers.
      */
-    
     public void increaseProduct(int corderid) {
 
         COrderItemDao coid = new COrderItemDao();
@@ -140,20 +177,18 @@ public class StorageServices {
     }
 
     /**
-     * Increase the quantities of a specific product that are available for 
+     * Increase the quantities of a specific product that are available for
      * customers and update this data in the database.
-     * 
-     * @param corderid - An integer that defines the id of an order from customers.
+     *
+     * @param corderid - An integer that defines the id of an order from
+     * customers.
      */
-    
     public void updateProduct(int corderid) {
-
         COrderItemDao coid = new COrderItemDao();
         List<COrderItem> corderitems = coid.getItemsPerCOrder(corderid);
         // corderitems - an arraylist that contains all the items in a specific 
         // order from customers.
         for (COrderItem cOrderItem : corderitems) {
-            
             updateIngredients(corderid, cOrderItem.getQuantity());
             // For each item in a specific order from customer, call the 
             // updateIngredients method and update the quantities of the 
@@ -162,12 +197,12 @@ public class StorageServices {
     }
 
     /**
-     * Decrease the quantities of a specific product that are available for 
+     * Decrease the quantities of a specific product that are available for
      * customers and update this data in the database.
-     * 
-     * @param corderid - An integer that defines the id of an order from customers.
+     *
+     * @param corderid - An integer that defines the id of an order from
+     * customers.
      */
-    
     public void decreaseProduct(int corderid) {
 
         COrderItemDao coid = new COrderItemDao();
@@ -176,18 +211,18 @@ public class StorageServices {
         // order from customers.
         ProductDao pd = new ProductDao();
         for (COrderItem cOrderItem : corderitems) {
-            
+
             Product productFromOrder = cOrderItem.getProduct();
-            if (cOrderItem.getQuantity() <= pd.getById(productFromOrder.getId()).getQuantity()){
-            // Check if the quantity of a specific product in an order from customers 
-            // is less or equal to the available quantity of this product in the storage.
-            // Check if there are enough products in the storage.
+            if (cOrderItem.getQuantity() <= pd.getById(productFromOrder.getId()).getQuantity()) {
+                // Check if the quantity of a specific product in an order from customers 
+                // is less or equal to the available quantity of this product in the storage.
+                // Check if there are enough products in the storage.
                 pd.updateQuantity(productFromOrder.getId(), pd.getById(productFromOrder.getId()).getQuantity() - cOrderItem.getQuantity());
                 // If there are enough products in the storage, decrease the 
                 // quantity of this product by the quantity given in the order 
                 // from customers and update the database.
             } else {
-                
+
                 JOptionPane.showMessageDialog(null, "Not enough products in the storage.");
             }
         }
--- proderp/src/test/java/gr/aueb/dmst/pijavaparty/proderp/services/StorageServicesTest.java
+++ proderp/src/test/java/gr/aueb/dmst/pijavaparty/proderp/services/StorageServicesTest.java
@@ -6,9 +6,6 @@
 package gr.aueb.dmst.pijavaparty.proderp.services;
 
 import java.util.function.Function;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -26,114 +23,91 @@ public class StorageServicesTest {
     public static void setUpClass() {
     }
     
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Test of StorageServices method, of class StorageServices.
-     */
-    @Test
-    public void testStorageServices() {
-        System.out.println("StorageServices");
-        StorageServices instance = new StorageServices();
-        instance.StorageServices();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    /**
+//     * Test of updateIngredients method, of class StorageServices.
+//     */
+//    @Test
+//    public void testUpdateIngredients() {
+//        System.out.println("updateIngredients");
+//        int id = 0;
+//        int quant = 0;
+//        StorageServices instance = new StorageServices();
+//        boolean expResult = false;
+//        boolean result = instance.updateIngredients(id, quant);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
-    /**
-     * Test of updateIngredients method, of class StorageServices.
-     */
-    @Test
-    public void testUpdateIngredients() {
-        System.out.println("updateIngredients");
-        int id = 0;
-        int quant = 0;
-        StorageServices instance = new StorageServices();
-        boolean expResult = false;
-        boolean result = instance.updateIngredients(id, quant);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of permissionToUpdateIngredients method, of class StorageServices.
-     */
-    @Test
-    public void testPermissionToUpdateIngredients() {
-        System.out.println("permissionToUpdateIngredients");
-        int id = 0;
-        int quant = 0;
-        StorageServices instance = new StorageServices();
-        boolean expResult = false;
-        boolean result = instance.permissionToUpdateIngredients(id, quant);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateQuantityOfRawMaterialBy method, of class StorageServices.
-     */
-    @Test
-    public void testUpdateQuantityOfRawMaterialBy() {
-        System.out.println("updateQuantityOfRawMaterialBy");
-        int q = 0;
-        StorageServices instance = new StorageServices();
-        Function<Integer, Integer> expResult = null;
-        Function<Integer, Integer> result = instance.updateQuantityOfRawMaterialBy(q);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of increaseProduct method, of class StorageServices.
-     */
-    @Test
-    public void testIncreaseProduct() {
-        System.out.println("increaseProduct");
-        int corderid = 0;
-        StorageServices instance = new StorageServices();
-        instance.increaseProduct(corderid);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateProduct method, of class StorageServices.
-     */
-    @Test
-    public void testUpdateProduct() {
-        System.out.println("updateProduct");
-        int corderid = 0;
-        StorageServices instance = new StorageServices();
-        instance.updateProduct(corderid);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of decreaseProduct method, of class StorageServices.
-     */
-    @Test
-    public void testDecreaseProduct() {
-        System.out.println("decreaseProduct");
-        int corderid = 0;
-        StorageServices instance = new StorageServices();
-        instance.decreaseProduct(corderid);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
+//    /**
+//     * Test of permissionToUpdateIngredients method, of class StorageServices.
+//     */
+//    @Test
+//    public void testPermissionToUpdateIngredients() {
+//        System.out.println("permissionToUpdateIngredients");
+//        int id = 0;
+//        int quant = 0;
+//        StorageServices instance = new StorageServices();/
+//        boolean expResult = false;
+//        boolean result = instance.permissionToUpdateIngredients(id, quant);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of updateQuantityOfRawMaterialBy method, of class StorageServices.
+//     */
+//    @Test
+//    public void testUpdateQuantityOfRawMaterialBy() {
+//        System.out.println("updateQuantityOfRawMaterialBy");
+//        int q = 0;
+//        StorageServices instance = new StorageServices();
+//        Function<Integer, Integer> expResult = null;
+//        Function<Integer, Integer> result = instance.updateQuantityOfRawMaterialBy(q);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of increaseProduct method, of class StorageServices.
+//     */
+//    @Test
+//    public void testIncreaseProduct() {
+//        System.out.println("increaseProduct");
+//        int corderid = 0;
+//        StorageServices instance = new StorageServices();
+//        instance.increaseProduct(corderid);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of updateProduct method, of class StorageServices.
+//     */
+//    @Test
+//    public void testUpdateProduct() {
+//        System.out.println("updateProduct");
+//        int corderid = 0;
+//        StorageServices instance = new StorageServices();
+//        instance.updateProduct(corderid);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of decreaseProduct method, of class StorageServices.
+//     */
+//    @Test
+//    public void testDecreaseProduct() {
+//        System.out.println("decreaseProduct");
+//        int corderid = 0;
+//        StorageServices instance = new StorageServices();
+//        instance.decreaseProduct(corderid);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//    
 }

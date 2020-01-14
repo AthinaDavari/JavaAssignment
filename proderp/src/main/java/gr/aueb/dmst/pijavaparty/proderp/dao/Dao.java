6bf998b6bc8bbdb144d40dc5f6e6183e86dd5c64
= <56390370+mariakokkorou@users.noreply.github.com>
2020-01-14 16:06:54 +0200

    Update help.html

--- proderp/src/main/java/gr/aueb/dmst/pijavaparty/proderp/dao/Dao.java
+++ proderp/src/main/java/gr/aueb/dmst/pijavaparty/proderp/dao/Dao.java
@@ -39,7 +39,7 @@ public class Dao {
             try {
                 String url = "jdbc:mysql://localhost:3306/" + dbname + "?serverTimezone=Etc/GMT-2";
 
-                conn = DriverManager.getConnection(url, "root", "12345");
+                conn = DriverManager.getConnection(url, "root", "CUjpQbA^64");
 
 
 
